5246b7d379b54b437017a99f7b0870f7c46cfcef
Nick <nick@nickswalker.com>
2012-07-30 09:42:34 -0500

    Add Basic Testing
    - Stores values in counter XML
    - Shows random two-up

--- crammer.php
+++ crammer.php
@@ -1,7 +1,34 @@
 <?php
 class Crammer
 {			
+	function showTest(){
+		if(rand(0,1)){
+		echo( $this->vars['test_term_name'].'<a class="button left" href="?term='.$this->vars['correct_index'].'&answer=1">'.$this->vars['correct_definition'].'</a><a class="button right" href="?term='.$this->vars['correct_index'].'&answer=0">'.$this->vars['incorrect_definition'].'</a>');
+		}
+		else{
+			echo( $this->vars['test_term_name'].'<a class="button left" href="?term='.$this->vars['correct_index'].'&answer=0">'.$this->vars['incorrect_definition'].'</a><a class="button right" href="?term='.$this->vars['correct_index'].'&answer=1">'.$this->vars['correct_definition'].'</a>');
+		}
+	}
+	function storeAnswer($term, $answer){
+
+		if(file_exists('store.xml')){
+			$terms_xml = simplexml_load_file('store.xml');
+			//If correct, increment the counter for that term
+			if($answer){
+				 $terms_xml->term[(int)$term]->counter = ++$terms_xml->term[(int)$term]->counter;
+				
+			}
+			else{
+				$terms_xml->term[(int)$term]->counter = --$terms_xml->term[(int)$term]->counter;
+			}
+		}
+		$terms_xml->asXML('store.xml');
+
+	}
 	function initialize(){
+	if (isset($_GET['answer']) && isset($_GET['term']) ) {
+		$this->storeAnswer($_GET['term'],$_GET['answer']);
+	}
 	/*
 		$ch = curl_init('https://api.quizlet.com/2.0/sets/12226611?access_token=NGY0M2U2NjRiYTMyYzZmYmYzYmQwMDBkYjY4NzFm&whitespace=1');
 		curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
@@ -25,10 +52,10 @@ class Crammer
 				$term_xml = $terms_xml->addChild('term');
 				$term_name_xml = $term_xml->addChild('name');
 				$definition_xml = $term_xml->addChild('definition');
-
+				$counter_xml = $term_xml->addChild('counter');
 				$term_xml->name = (string)$term->term;
 				$term_xml->definition = (string)$term->definition;
-
+				$term_xml->counter = 0;
 			}
 			$terms_xml->asXML('store.xml');
 		
@@ -41,11 +68,7 @@ class Crammer
 		$this->vars['correct_definition'] = (string)$this->vars['test_term']->definition;
 		$this->vars['incorrect_term'] = $terms_xml->term[$this->vars['incorrect_index']];
 		$this->vars['incorrect_definition'] = (string)$this->vars['incorrect_term']->definition;
-		print_r($this->vars);
-	}
-	function showTest(){
-		
-		echo( $this->vars['test_term_name'].$this->vars['correct_definition'].$this->vars['incorrect_definition']);
+		//print_r($this->vars);
 	}
 }
 ?>
--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <html>
   <head>
     <meta charset='utf-8' />
-    <meta name="description" content="NeueGal : A simple, no-database PHP image gallery." />
+    <meta name="description" content="Crammer: Dead simple study tool." />
     <link rel="stylesheet" media="screen" href="../global.css">
 
     <title>Crammer</title>
