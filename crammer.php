d00e1f8e354565e054aee45a8d486d5e7d4aef05
Nick <nick@nickswalker.com>
2012-08-06 19:59:19 -0500

    Clarify some variable,class names
    - Add title and description to XML file
    - Rejigger xml structure to accommodate metadata

--- crammer.php
+++ crammer.php
@@ -2,7 +2,7 @@
 class Crammer
 {
 	function generateQuestions($numberOfQuestions = 1, $numberOfChoices = 2){
-		echo( '<div class="test"><h1 data-set="'. $this->vars['set'] .'"data-term="'.$this->vars['correct_index'].'">'.$this->vars['test_term_name'].'</h1>'. $this->generateChoices($numberOfChoices) .'</div>');
+		echo( '<div class="test"><h1 data-set="'. $this->vars['set'] .'"data-index="'.$this->vars['correct_index'].'">'.$this->vars['correct_term_name'].'</h1>'. $this->generateChoices($numberOfChoices) .'</div>');
 
 	}
 	function generateChoices($numberOfChoices = 2){
@@ -10,7 +10,7 @@ class Crammer
 		$correctSpot = rand(1,(int)$numberOfChoices);
 		for ($i = 1; $i <= $numberOfChoices; $i++) {
 			if($i == $correctSpot){
-				$output .= '<a class="button" data-answer="1" href="">'. $this->vars['correct_definition'] .'</a>';
+				$output .= '<a class="choice" data-correct="true" href="" data-definition="'. $this->vars['correct_term_definition'] .'">'. $this->vars['correct_term_definition'] .'</a>';
 			}
 			else{
 				$temp_incorrect_index = rand(0,$this->vars['number_of_terms']);
@@ -18,7 +18,8 @@ class Crammer
 					$temp_incorrect_index = rand(0,$this->vars['number_of_terms']);
 				}
 				$temp_incorrect_definition = (string)$this->vars['terms_xml']->term[$temp_incorrect_index]->definition;
-				$output .= '<a class="button" data-answer="0" href="">'.$temp_incorrect_definition.'</a>';
+				$temp_incorrect_name = (string)$this->vars['terms_xml']->term[$temp_incorrect_index]->name;
+				$output .= '<a class="choice" data-correct="false" data-name="'.$temp_incorrect_name.'" href="">'.$temp_incorrect_definition.'</a>';
 			}
 		}
 		return $output;
@@ -26,57 +27,30 @@ class Crammer
 	function showError(){
 		require('error.php');
 	}
-	function storeAnswer($term, $answer, $slow){
+	function storeAnswer($index, $correct, $slow){
 		//If correct, increment the counter for that term
-		if(!$answer){
-			$this->vars['terms_xml']->term[(int)$term]->counter = --$this->vars['terms_xml']->term[(int)$term]->counter;
+		if($correct =='false'){
+			--$this->vars['terms_xml']->term[(int)$index]->counter;
 		}
 		else{
-			if($slow){
-				/* $terms_xml->term[(int)$term]->counter = $terms_xml->term[(int)$term]->counter - .5; */
+			if($slow == "true"){
+				/* $this->vars['terms_xml']->term[(int)$index]->counter -= .5; */
 				return false;
 			}
 			else{
-				$this->vars['terms_xml']->term[(int)$term]->counter = ++$this->vars['terms_xml']->term[(int)$term]->counter;
+				++$this->vars['terms_xml']->term[(int)$index]->counter;
 			}
 
 		}
-		$terms_xml->asXML('cache/'.$this->vars['set'].'.xml');
+		echo($this->vars['terms_xml']->term[(int)$index]->counter);
+		$this->vars['set_xml']->asXML('cache/'.$this->vars['set'].'.xml');
 		return true;
-	}
-	function initialize(){
-		if ( isset($_GET['set'])  ) {
-			$temp_success = $this->pickSet($_GET['set']);
-		}
-		elseif( isset($_POST['set']) ){
-			$temp_success = $this->pickSet($_POST['set']);
-		}
-		else {
-			require('picker.php');
-			return false;
-		}
-		if ($temp_success){
-			$this->setVars();
-			// Store an answer if we get the relevant data
-			if ( isset($_POST['answer']) && isset($_POST['term']) && isset($_POST['slow']) ) {
-				$this->storeAnswer($_POST['term'],$_POST['answer'],$_POST['slow']);
-				return false;
-			}
-			// Generate a question if we get the relevant data
-			elseif ( isset($_POST['questions']) && isset($_POST['choices']) ) {
-				$this->generateQuestions($_POST['questions'],$_POST['choices']);
-				return false;
-			}
-			else{
-				require('tester.php');
-			}
-		}
 	}
 	function pickSet($id){
 		$this->vars['set'] = $id;
 		if( file_exists('cache/'.$this->vars['set'].'.xml') ){
-			$terms_xml = simplexml_load_file('cache/'.$this->vars['set'].'.xml');
-			$this->vars['terms_xml'] = $terms_xml;
+			$set_xml = simplexml_load_file('cache/'.$this->vars['set'].'.xml');
+			$this->vars['set_xml'] = $set_xml;
 			return true;
 		}
 		else{
@@ -98,32 +72,63 @@ class Crammer
 				$this->showError();
 				return false;
 			}
-			$xmlstr = "<?xml version='1.0' ?><terms></terms>";
-			$terms_xml = new SimpleXMLElement($xmlstr);
+			$xmlstr = "<?xml version='1.0' ?><set><details></details><terms></terms></set>";
+			$set_xml = new SimpleXMLElement($xmlstr);
+			$title_xml = $set_xml->details->addChild('title');
+			$description_xml = $set_xml->details->addChild('description');
+			$set_xml->details->title = (string)$json->title;
+			$set_xml->details->description = (string)$json->description;
 			foreach ($terms as $term) {
-				$term_xml = $terms_xml->addChild('term');
+				$term_xml = $set_xml->terms->addChild('term');
 				$term_name_xml = $term_xml->addChild('name');
 				$definition_xml = $term_xml->addChild('definition');
 				$counter_xml = $term_xml->addChild('counter');
 				$term_xml->name = htmlspecialchars((string)$term->term);
 				$term_xml->definition = htmlspecialchars((string)$term->definition);
-				/* $term_xml->counter = 0; */
+				$term_xml->counter = 0;
 			}
-			$terms_xml->asXML('cache/'.$this->vars['set'].'.xml');
-			$this->vars['terms_xml'] = $terms_xml;
+			$set_xml->asXML('cache/'.$this->vars['set'].'.xml');
+			$this->vars['set_xml'] = $set_xml;
+			
 			return true;
 
 		}
 	}
 	function setVars(){
-
+		$this->vars['terms_xml'] = $this->vars['set_xml']->terms;
 		$this->vars['number_of_terms'] = $this->vars['terms_xml']->term->count() - 1;
 		$this->vars['correct_index'] =  rand(0,$this->vars['number_of_terms']);
-		$this->vars['test_term'] = $this->vars['terms_xml']->term[$this->vars['correct_index']];
-		$this->vars['test_term_name'] = (string)$this->vars['test_term']->name;
-		$this->vars['correct_definition'] = (string)$this->vars['test_term']->definition;
-
-
+		$this->vars['correct_term'] = $this->vars['terms_xml']->term[$this->vars['correct_index']];
+		$this->vars['correct_term_name'] = (string)$this->vars['correct_term']->name;
+		$this->vars['correct_term_definition'] = (string)$this->vars['correct_term']->definition;
+	}
+	function initialize(){
+		if ( isset($_GET['set'])  ) {
+			$temp_success = $this->pickSet($_GET['set']);
+		}
+		elseif( isset($_POST['set']) ){
+			$temp_success = $this->pickSet($_POST['set']);
+		}
+		else {
+			require('picker.php');
+			return false;
+		}
+		if ($temp_success){
+			$this->setVars();
+			// Store an answer if we get the relevant data
+			if ( isset($_POST['index']) && isset($_POST['correct']) && isset($_POST['slow']) ) {
+				$this->storeAnswer($_POST['index'],$_POST['correct'],$_POST['slow']);
+				return false;
+			}
+			// Generate a question if we get the relevant data
+			elseif ( isset($_POST['questions']) && isset($_POST['choices']) ) {
+				$this->generateQuestions($_POST['questions'],$_POST['choices']);
+				return false;
+			}
+			else{
+				require('tester.php');
+			}
+		}
 	}
 }
 ?>
--- test-interface.js
+++ test-interface.js
@@ -8,9 +8,7 @@ $(document).ready(function() {
 		slowToggled = false,
 		wrongToggled = false,
 		timer = null;
-		
 	getQuestions();
-	
 	$('body').keyup(function(event) {
 		switch (event.keyCode) {
 		case 87:
@@ -27,14 +25,16 @@ $(document).ready(function() {
 		event.preventDefault();
 		clearTimeout(timer);
 		$('.test').removeClass('current');
+		getQuestions();
 		postAnswer($(this));
 	});
-	$incorrectCounter.on('click', function(event){
+	$incorrectCounter.on('click', function(event) {
 		toggleWrong();
 	});
-	$slowCounter.on('click', function(event){
+	$slowCounter.on('click', function(event) {
 		toggleSlow();
 	});
+
 	function toggleWrong() {
 		if (slowToggled) {
 			$('.incorrect').slideToggle(200);
@@ -59,17 +59,15 @@ $(document).ready(function() {
 		slowToggled = !slowToggled;
 	}
 
-
 	function updateStats(isCorrect) {
 		if (!isCorrect) {
 			$incorrectCounter.data().counter++;
 			$incorrectCounter.text(String($incorrectCounter.data().counter));
 		} else {
-			if(slow){
+			if (slow) {
 				$slowCounter.data().counter++;
 				$slowCounter.text(String($slowCounter.data().counter));
-			}
-			else{
+			} else {
 				$correctCounter.data().counter++;
 				$correctCounter.text(String($correctCounter.data().counter));
 			}
@@ -108,7 +106,7 @@ $(document).ready(function() {
 	}
 
 	function postAnswer($clicked) {
-		if (!$clicked.data().answer) {
+		if (!$clicked.data().correct) {
 			$currentTest.addClass('incorrect');
 			updateStats(0);
 		} else {
@@ -118,14 +116,14 @@ $(document).ready(function() {
 				$currentTest.addClass('slow');
 			}
 		}
-		getQuestions();
+
 		var testData = $clicked.siblings('h1').data(),
-			term = testData.term,
-			answer = $clicked.data().answer,
+			index = testData.index,
+			correct = $clicked.data().correct,
 			set = $crammerData.set,
 			data = {
-				term: term,
-				answer: answer,
+				index: index,
+				correct: correct,
 				set: set,
 				slow: slow
 			};
@@ -134,7 +132,9 @@ $(document).ready(function() {
 			url: "index.php",
 			data: data,
 			dataType: "text",
-			success: function(returnedObject) {}
+			success: function(returnedObject) {
+				console.log(returnedObject);
+			}
 		});
 	}
 });
